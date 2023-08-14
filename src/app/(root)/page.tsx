3c42461110a0d84dc8cff06d5dddd11075490db6
Jeff Derbes <derbes.jeffrey@gmail.com>
2023-08-14 06:25:25 -0500

    quip card and fetch quips on homepage

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,37 @@
 //app/page.tsx
+import { currentUser } from "@clerk/nextjs";
+import { fetchQuips } from "@/lib/actions/quip.actions";
+import QuipCard from "@/components/cards/QuipCard";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchQuips(1, 30);
+  const user = await currentUser();
+
   return (
     <>
       <h1 className={"head-text text-left"}>Home</h1>
+
+      <section className={"mt-9 flex flex-col gap-10"}>
+        {result.quips.length === 0 ? (
+          <p className={"no-result"}>No quips found</p>
+        ) : (
+          <>
+            {result.quips.map((quip) => (
+              <QuipCard
+                key={quip._id}
+                id={quip._id}
+                currentUserId={user?.id || ""}
+                parentId={quip.parentId}
+                content={quip.text}
+                author={quip.author}
+                community={quip.community}
+                createdAt={quip.createdAt}
+                comments={quip.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
     </>
   );
 }
--- src/lib/actions/quip.actions.ts
+++ src/lib/actions/quip.actions.ts
@@ -10,6 +10,42 @@ interface Params {
   path: string;
 }
 
+export async function fetchQuips(pageNumber = 1, pageSize = 20) {
+  await connectToDB();
+
+  // Calculate the number of quips to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Create a query to fetch the quips that have no parent (top-level quips) (a quips that is not a comment/reply).
+  const quipsQuery = Quip.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    .populate({
+      path: "children", // Populate the children field
+      populate: {
+        path: "author", // Populate the author field within children
+        model: User,
+        select: "_id name parentId image", // Select only _id and username fields of the author
+      },
+    });
+
+  // Count the total number of top-level posts (quiips) i.e., quips that are not comments.
+  const totalQuipsCount = await Quip.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
+
+  const quips = await quipsQuery.exec();
+
+  const isNext = totalQuipsCount > skipAmount + quips.length;
+
+  return { quips, isNext };
+}
+
 export async function createQuip({ text, author, path, communityId }: Params) {
   try {
     await connectToDB();
