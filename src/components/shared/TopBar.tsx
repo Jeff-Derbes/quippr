1fec43cae979a3c3678f326fdfaaa8a2fdec703b
Jeff Derbes <derbes.jeffrey@gmail.com>
2023-08-15 22:12:37 -0500

    update logo

--- src/components/shared/TopBar.tsx
+++ src/components/shared/TopBar.tsx
@@ -7,7 +7,13 @@ const TopBar = () => {
   return (
     <nav className={"topbar"}>
       <Link href={"/"} className={"flex items-center gap-4"}>
-        <Image src={"/assets/logo.svg"} alt={"logo"} width={28} height={28} />
+        <Image
+          src={"/assets/quippr-no-bg.svg"}
+          alt={"logo"}
+          width={125}
+          height={125}
+          className={"absolute mt-4"}
+        />
         <p className={"text-heading3-bold text-light-1 max-xs hidden"}>
           Quippr
         </p>
